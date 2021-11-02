5d636942c88c342f8c95b3f0b0a19fa457c82ee6
Alexandr Shabalkin <sashatlt8@gmail.com>
2021-11-03 01:32:17 +0400

    The redirect was removed with an incorrect request

--- api/urls.py
+++ api/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
+    # re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
